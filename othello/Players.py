a5c51c2eef4b609de35108ed50a623c30957f35d
Lucas Dunn <lkdunn@gmail.com>
2023-05-14 17:33:23 -0700

    Coded successor and alphabeta search successfully, with buggy eval functions

--- othello/Players.py
+++ othello/Players.py
@@ -1,3 +1,6 @@
+from OthelloBoard import OthelloBoard 
+
+
 class Player:
     """Base player class"""
     def __init__(self, symbol):
@@ -77,36 +80,137 @@ class AlphaBetaPlayer(Player):
             return "X"
 
 
+    #returns the utility value, column, and row of the best move 
+    #symbol will always be self.symbol
+    def max_value(self,board,alpha,beta,depth): #returns (value,col,row) 
+        self.total_nodes_seen+=1
+        if (not board.has_legal_moves_remaining(self.symbol) or depth == self.max_depth):
+            return (self.eval_board(board), 0, 0) #returns the utility value and an arbitrary "move"
+        bestValue = -17
+        bestColumn = 0
+        bestRow = 0
+        successors = self.get_successors(board,self.symbol)
+        for (newBoard,c,r) in successors:
+            tempValue = self.min_value(newBoard,alpha,beta,depth+1)
+            if (tempValue > bestValue):
+                bestValue = tempValue
+                bestColumn = c
+                bestRow = r
+            if (bestValue >= beta): #we can prune this branch
+                return (bestValue,c,r) #this branch will be pruned
+            if (bestValue> alpha): #we have found a better move
+                alpha = bestValue
+        return (bestValue,bestColumn,bestRow)
+
+    #returns the utility value, column, and row of the opponents best move 
+    #symbol will always be self.oppSym
+    def min_value(self,board,alpha,beta,depth): #returns (value,col,row)
+        self.total_nodes_seen+=1
+        if (not board.has_legal_moves_remaining(self.oppSym) or depth == self.max_depth):
+            return self.eval_board(board) #returns the utility value and an arbitrary "move"
+        oppBestValue = 17
+        successors = self.get_successors(board,self.oppSym)
+        for (newBoard,c,r) in successors:
+            (tempValue,tempR,tempC) = self.max_value(newBoard,alpha,beta,depth+1)
+            if (tempValue < oppBestValue):
+                oppBestValue = tempValue
+            if (oppBestValue <= alpha): #we can prune this branch
+                return oppBestValue #this branch will be pruned
+            if (oppBestValue < beta): #we have found a better move
+                beta = oppBestValue
+        return oppBestValue
+
+
     def alphabeta(self, board):
         # Write minimax function here using eval_board and get_successors
         # type:(board) -> (int, int)
         col, row = 0, 0
-        return col, row
+        (value, col, row) = self.max_value(board,-17,17,0)
+        return (col, row)
 
 
+
+    """THIS IS THE BUGGY PART OF THE CODE"""
     def eval_board(self, board):
         # Write eval function here
         # type:(board) -> (float)
         value = 0
-        if self.eval_type == 0:
-            value = 0
-        elif self.eval_type == 1:
-            value = 1
-        elif self.eval_type == 2:
-            value = 2
+        #print("My eval type: ", str(self.eval_type))
+        if (self.eval_type == 0): #Piece Difference
+            #print("Calcing piece difference")
+            myPieces = 0
+            opposingPieces = 0
+            for c in range(board.get_num_cols()):
+                for r in range(board.get_num_rows()):
+                    if (board.get_cell(c,r) == self.symbol):
+                        myPieces += 1
+                    elif (board.get_cell(c,r) == self.oppSym):
+                        opposingPieces  += 1
+            #print("Mine: " + str(myPieces) + "Opp: " + str(opposingPieces))
+            value = myPieces - opposingPieces     
+        elif (self.eval_type == 1): #Mobility
+            myMoves = 0
+            opposingMoves = 0
+            for c in range(board.get_num_cols()):
+                for r in range(board.get_num_rows()):
+                    if (board.is_legal_move(c,r,self.symbol)):
+                        myMoves += 1
+                    elif (board.is_legal_move(c,r,self.oppSym)):
+                        opposingMoves  += 1
+            value = myMoves - opposingMoves
+        elif (self.eval_type == 2): #Own Function: 3*corner pieces + 1*legal corner move - ...
+            myScore = 0
+            opposingScore = 0
+            #0,0 ; 0,row ; col, o; row, col
+            c = 0
+            r = 0
+            while 1: #Loop for each corner of the board
+                if (board.get_cell(c,r) == self.symbol):
+                    myScore += 3
+                elif (board.get_cell(c,r) == self.oppSym):
+                    opposingScore  += 3
+                if (board.is_legal_move(c,r,self.symbol)):
+                    myScore += 1
+                elif (board.is_legal_move(c,r,self.oppSym)):
+                    opposingScore += 1
+                if c==0:
+                    if r==0:
+                        r=board.get_num_rows()-1
+                    else:
+                        c=board.get_num_cols()-1
+                        r=0
+                else:
+                    if r==0:
+                       r=board.get_num_rows()-1
+                    else:
+                        break 
+            value = myScore - opposingScore
         return value
+
 
 
     def get_successors(self, board, player_symbol):
         # Write function that takes the current state and generates all successors obtained by legal moves
         # type:(board, player_symbol) -> (list)
-        successors = []
+        successors = [] #stores (board,column,row)
+        for c in range(board.get_num_cols()):
+            for r in range(board.get_num_rows()):
+                if board.is_legal_move(c,r,player_symbol):
+                    newBoard = board.cloneOBoard()
+                    newBoard.play_move(c,r,player_symbol)
+                    successors.append((newBoard,c,r))      
         return successors 
 
 
     def get_move(self, board):
         # Write function that returns a move (column, row) here using minimax
         # type:(board) -> (int, int)
+
+
+        """FOR TESTING PURPOSES:"""
+        #print("Current Boards evaluation: " + str(self.eval_board(board)) + "\n")
+        #exit(0)
+
         return self.alphabeta(board)
 
        
